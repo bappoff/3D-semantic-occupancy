b024c22aa3ef15b3bc2e79c88b01bfb515373884
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-25 10:02:24 +0200

    Update visualization_chat.py

--- scripts/visualization_chat.py
+++ scripts/visualization_chat.py
@@ -10,13 +10,16 @@ with open('/work/scitas-share/voxformer/VoxFormer/3D-semantic-occupancy/results/
 y_pred_list = [d['y_pred'] for d in data]
 y_true_list = [d['y_true'] for d in data]
 
+# Select a slice index to visualize
+slice_index = 16
+
 # Visualize the arrays
 fig, axes = plt.subplots(len(y_pred_list), 2, figsize=(10, 10))
 
 for i, (y_pred, y_true) in enumerate(zip(y_pred_list, y_true_list)):
-    axes[i, 0].imshow(np.squeeze(y_pred))
+    axes[i, 0].imshow(y_pred[:, :, slice_index])
     axes[i, 0].set_title(f'Prediction {i+1}')
-    axes[i, 1].imshow(np.squeeze(y_true))
+    axes[i, 1].imshow(y_true[:, :, slice_index])
     axes[i, 1].set_title(f'True {i+1}')
 
 plt.tight_layout()

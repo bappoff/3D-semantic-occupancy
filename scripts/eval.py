40f54d86189b2ddaefa4a2f83027c0d1415735bd
bappoff <93776964+bappoff@users.noreply.github.com>
2023-05-25 15:04:58 +0200

    Update eval.py

--- scripts/eval.py
+++ scripts/eval.py
@@ -233,7 +233,7 @@ def main():
             broadcast_buffers=False)
         outputs = custom_multi_gpu_test(model, data_loader, args.tmpdir,
                                         args.gpu_collect)
-        breakpoint()
+
 
     rank, _ = get_dist_info()
     if rank == 0:
